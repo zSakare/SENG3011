ceb9df7e0fd996eabe39bb27a04b4a4880faa20b
Sohaib Mushtaq <msmu890@cse.unsw.edu.au>
2013-04-17 13:28:02 +1000

    added random time things

---
+++ src/main/engine/ClockTestDrive.java
@@ -0,0 +1,12 @@
+package main.engine;
+
+public class ClockTestDrive {
+	public static void main(String[] args) {
+		clock c = new clock();
+		int i=0;
+		for(i=0; i < 1000; i++){
+		c.randomizeClockTime();
+		}
+	
+	}
+}
