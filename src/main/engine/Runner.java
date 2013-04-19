f8d9b387541370a58a2ae9f60325ddcd3cfb6c16
Sohaib Mushtaq <msmu890@cse.unsw.edu.au>
2013-04-19 14:17:09 +1000

    Added in command line interface

--- src/main/engine/Runner.java
+++ src/main/engine/Runner.java
@@ -1,9 +1,22 @@
 package main.engine;
 
 import main.parser.SircaCSVParser;
+import java.util.Scanner;
+
 
 public class Runner {
 	public static void main(String[] argv) {
 		SircaCSVParser.input();
+		Scanner keyIn = new Scanner(System.in);
+		System.out.println("Please Choose a strategy ('R' for random)");
+		String c = keyIn.next();
+		System.out.println("Please Enter quantity to trade");
+		String quantity = keyIn.next();
+		System.out.println("Simulating...");
+		//newAskOrder(quantity);
+		//newBidOrder(quantity);
+		System.out.println("Evaluating strategy");
+		//double evaluation = evaluateStrategy();
+		//System.out.println("You made a profit of: " + evaluation);
 	}
 }
