fd8477a5d284851a3b7b23a12b40cf5b78279ad3
michael.vuong <mvuo803@cse.unsw.edu.au>
2013-05-13 16:22:28 +1000

    Resolving merge conflicts

--- src/test/implementations/OrderbookImplTest.java
+++ src/test/implementations/OrderbookImplTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import main.implementations.OrderBuilderImpl;
 import main.implementations.OrderbookImpl;
+import main.implementations.order.AlgorithmicTrade;
 import main.implementations.order.Order;
 import main.interfaces.OrderBuilder;
 import main.utils.Strategy;
@@ -26,13 +27,12 @@ public class OrderbookImplTest {
 	private List<Order> bidMeanRevList;
 	private List<Order> askMeanRevList;
 	
-	private List<Order> randomTradeList;
 	private List<Order> momentumTradeList;
 	private List<Order> meanRevTradeList;
 	
 	private static String instrument = "DEF";
 	private static Date date;
-	private static double price; 
+	private static double price = 150.00; 
 	private static String volume;
 	private static String ORDER_BID = "B";
 	private static String ORDER_ASK = "A";
@@ -291,51 +291,154 @@ public class OrderbookImplTest {
 		System.out.println("Testing Complete.");
 	}
 
+	/**
+	 * Tests the Random Strategy by checking if the volume for the
+	 * generated Bids and Asks orders are the same. 
+	 * It also checks whether list provided after running the 
+	 * strategy is not empty.
+	 */
 	@Test
 	public void testRunRandomStrategy() {
-		// test if someting in there
-		// check if volume is same
-		// test 1 new buy and ask bid
 		
-		System.out.println("Testing Random Strategy");
+		System.out.println("Testing Random Strategy...");
 		
 		OrderbookImpl randomOrderBook = new OrderbookImpl(randomBidList, 
 				randomAskList, 
 				null);
 		Strategy strategy = Strategy.RANDOM;
+		String randVolume = "999";
 		
-		randomOrderBook.runStrategy(strategy, "999");
+		List<AlgorithmicTrade> randomTradeList = randomOrderBook.runStrategy(strategy, randVolume);
+		assertNotNull("Checking randomTradeList is not empty...", randomTradeList);
 		
-		
-		
-		
-		
-		fail("Not yet implemented");
+		for (AlgorithmicTrade algorithmicTrade : randomTradeList) {
+			if (algorithmicTrade.getBidOrder().getBuyerBrokerId() == "6969") {
+				// Checking volumes are the same (this has to be true)
+				assert (algorithmicTrade.getBidOrder().getVolume() == 999);
+			} else { 
+				assert (algorithmicTrade.getAskOrder().getVolume() == 999);
+			}
+		}
+		System.out.println("Random Strategy Test Passed.");
 	}
 	
+	/**
+	 * Tests the Momentum Strategy for a Positive trend.
+	 * It also checks whether list provided after running the 
+	 * strategy is not empty.
+	 * We also check to make sure that a Bid has been created to match
+	 * the positive trend.
+	 */
 	@Test
-	public void testRunMomentumStrategy() {
+	public void testPosMomentumStrategy() {
 		
-		// if volume to trade is the same 
-		// test if it's positive we buy
-		// negative we sell
+		System.out.println("Testing Positive Momentum Strategy...");
 		
-		fail("Not yet implemented");
+		OrderbookImpl momentumOrderbook = new OrderbookImpl (bidMomentumList, 
+				askMomentumList, momentumTradeList);
+		Strategy strategy = Strategy.MOMENTUM;
+		String randVolume = "999";
+		
+		List<AlgorithmicTrade> posMomentumTradeList = momentumOrderbook.runStrategy(strategy, randVolume);
+		assertNotNull("Checking MomentumTradeList is not empty...", posMomentumTradeList);
+		
+		for (AlgorithmicTrade algorithmicTrade : posMomentumTradeList) {
+			if (algorithmicTrade.getBidOrder().getBuyerBrokerId() == "6969") { 
+				assert (algorithmicTrade.getBidOrder().getVolume() == 999);
+			}
+		}
+		System.out.println("Positive Momentum Strategy Test Passed.");
 	}
 	
+	/**
+	 * Tests the Momentum Strategy for a Negative trend.
+	 * It also checks whether list provided after running the 
+	 * strategy is not empty.
+	 * We also check to make sure that an Ask has been created to match
+	 * the Negative trend.
+	 */
 	@Test
-	public void testRunMeanRevisionStrategy() {
+	public void testNegMomentumStrategy() {
 		
-		// same as moementum except 
-		// test if it's negative we buy
-		// if it's positive we sell
+		System.out.println("Testing Negative Momentum Strategy...");
 		
-		fail("Not yet implemented");
+		OrderbookImpl momentumOrderbook = new OrderbookImpl (bidMomentumList, 
+				askMomentumList, momentumTradeList);
+		Strategy strategy = Strategy.MOMENTUM;
+		String randVolume = "999";
+		
+		List<AlgorithmicTrade> negMomentumTradeList = momentumOrderbook.runStrategy(strategy, randVolume);
+		assertNotNull("Checking MomentumTradeList is not empty...", negMomentumTradeList);
+		
+		for (AlgorithmicTrade algorithmicTrade : negMomentumTradeList) {
+			if (algorithmicTrade.getAskOrder().getSellerBrokerId() == "6969") { 
+				assert (algorithmicTrade.getAskOrder().getVolume() == 999);
+			}
+		}
+		System.out.println("Negative Momentum Strategy Test Passed.");
+	}
+	
+	/**
+	 * Tests the Mean Revision Strategy for a Positive trend.
+	 * It also checks whether list provided after running the 
+	 * strategy is not empty.
+	 * We also check to make sure that an Ask has been created to match
+	 * the positive trend.
+	 */
+	@Test
+	public void testPosMeanRevStrategy() {
+		
+		System.out.println("Testing Positive Mean Revision Strategy...");
+		
+		OrderbookImpl meanRevOrderbook = new OrderbookImpl (bidMeanRevList, 
+				askMeanRevList, meanRevTradeList);
+		Strategy strategy = Strategy.MEAN_REVISION;
+		String randVolume = "999";
+		
+		List<AlgorithmicTrade> posMeanRevTradeList = meanRevOrderbook.runStrategy(strategy, randVolume);
+		assertNotNull("Checking MeanRevTradeList is not empty...", posMeanRevTradeList);
+		
+		for (AlgorithmicTrade algorithmicTrade : posMeanRevTradeList) {
+			if (algorithmicTrade.getAskOrder().getSellerBrokerId() == "6969") { 
+				assert (algorithmicTrade.getAskOrder().getVolume() == 999);
+			}
+		}
+		System.out.println("Positive Mean Revision Strategy Test Passed.");
+	}
+	
+	/**
+	 * Tests the Mean Revision Strategy for a Negative trend.
+	 * It also checks whether list provided after running the 
+	 * strategy is not empty.
+	 * We also check to make sure that a Bid has been created to match
+	 * the negative trend.
+	 */
+	@Test
+	public void testNegMeanRevStrategy() {
+		
+		System.out.println("Testing Negative Mean Revision Strategy...");
+		
+		OrderbookImpl meanRevOrderbook = new OrderbookImpl (bidMeanRevList, 
+				askMeanRevList, meanRevTradeList);
+		Strategy strategy = Strategy.MEAN_REVISION;
+		String randVolume = "999";
+		
+		List<AlgorithmicTrade> negMeanRevTradeList = meanRevOrderbook.runStrategy(strategy, randVolume);
+		assertNotNull("Checking MeanRevTradeList is not empty...", negMeanRevTradeList);
+		
+		for (AlgorithmicTrade algorithmicTrade : negMeanRevTradeList) {
+			if (algorithmicTrade.getAskOrder().getBuyerBrokerId() == "6969") { 
+				assert (algorithmicTrade.getBidOrder().getVolume() == 999);
+			}
+		}
+		System.out.println("Negative Mean Revision Strategy Test Passed.");
 	}
 
 	@Test
 	public void testTradeMatcher() {
-		fail("Not yet implemented");
+		
+		
+		
 	}
 
 }
